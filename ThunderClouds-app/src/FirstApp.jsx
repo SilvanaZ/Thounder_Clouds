6ef184fb225f6ef1afe0ca14fb36f63ba0705f98
Rocio <silvana.blaq@gmail.com>
2022-06-28 15:45:05 -0300

    comunicacion entre componntes

--- ThunderClouds-app/src/FirstApp.jsx
+++ ThunderClouds-app/src/FirstApp.jsx
@@ -1,15 +1,27 @@
 //import { Fragment } from "react";
-const newMessage = {
-  message: "Hola meme",
-  title: "mosho",
-};
 
-export const FirstApp = () => {
+/* const getSaludo = {
+  message: 10 / 3,
+  title: "Chipi",
+};
+export const FirstApp2 = () => {
   return (
     <>
-      <h1>Hola Mondo</h1>
+      <h1>Hola yo doy {getSaludo.message}</h1>
+      {<code>{JSON.stringify(newMessage)}</code>}
+      <p>Hey!! Soy un subtitulo con un {getSaludo.title}</p>
+    </>
+  );
+}; */
+
+export const FirstApp = (props) => {
+  console.log(props);
+
+  return (
+    <>
+      <h1>{props.title}</h1>
       {/*<code>{JSON.stringify(newMessage)}</code>*/}
-      <p>Soy un subtitulo</p>
+      <p>Hey!! Soy un subtitulo chipitense!!</p>
     </>
   );
 };
--- ThunderClouds-app/src/main.jsx
+++ ThunderClouds-app/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { FirstApp } from "./FirstApp";
 //import { LoveApp } from "./LoveApp";
 
+import "./styles.css";
+
 //ReactDOM.createRoot(document.getElementById("root")).render(
 //<React.StrictMode>
 //<LoveApp />
