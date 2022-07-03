ce54228180fa5b249c338933c824483c55d6bed1
Rocio <silvana.blaq@gmail.com>
2022-07-03 18:02:17 -0300

    Creating CounterApp

--- ThunderClouds-app/src/CounterApp.jsx
+++ ThunderClouds-app/src/CounterApp.jsx
@@ -1,10 +1,17 @@
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
+  const handleAdd = (event) => {
+    // console.log(event);
+    console.log('+1');
+    value += 1;
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> {value} </h2>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
@@ -12,3 +19,11 @@ export const CounterApp = ({ value }) => {
 CounterApp.propTypes = {
   value: PropTypes.number.isRequired,
 };
+
+/* <button onClick={(event) => handleAdd(event, "Hola Ro")}>
+        clickeame
+      </button>   -> esto va en el RETURN
+
+      const handleAdd = (event, newValue) => {
+  console.log(newValue); ->SE PIDE QUE VAYA FUERA DE LA FUNCION PERO LA PRUEBA FUE DENTRO DE FUNCION Y ANTES DEL RETURN
+};*/
